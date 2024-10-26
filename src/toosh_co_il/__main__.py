e5044701707de18324b8aaa32aefc7e49387c0fa
Mick <cutipus@protonmail.com>
2024-10-26 11:15:18 +0300

    init

---
+++ src/toosh_co_il/__main__.py
@@ -0,0 +1,6 @@
+def main():
+    print("Please run this using `flask --app snail_dot_faith run` instead!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/wsgi.py
@@ -0,0 +1,4 @@
+from toosh_co_il import app
+
+if __name__ == "__main__":
+    app.run()
