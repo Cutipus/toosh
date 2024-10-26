383feaa2b094ff7533169b69e258d2ff8a36db4b
Mick <cutipus@protonmail.com>
2024-10-26 15:59:14 +0300

    Allow focusing on an image preview to view it in full size

--- src/toosh_co_il/__init__.py
+++ src/toosh_co_il/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,3 +6,15 @@ app = Flask(__name__)
 @app.route("/")
 def index() -> str:
     return render_template("index.html", title="Toosh")
+
+
+@app.route("/item-focus")
+def item_focus() -> str:
+    item = request.args.get("item")
+    info = "a nice and big pic"
+    return render_template("item-focus.html", item=item, info=info)
+
+
+@app.route("/main-window")
+def main_window() -> str:
+    return render_template("main-window.html")
