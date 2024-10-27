38058daa13451704854ea7621c389c4541fdcae9
Mick <cutipus@protonmail.com>
2024-10-27 14:47:56 +0200

    Fix reference to snail_dot_faith project

--- src/toosh_co_il/__main__.py
+++ src/toosh_co_il/__main__.py
@@ -1,5 +1,5 @@
 def main():
-    print("Please run this using `flask --app snail_dot_faith run` instead!")
+    print("Please run this using `flask --app toosh_co_il run` instead!")
 
 
 if __name__ == "__main__":
