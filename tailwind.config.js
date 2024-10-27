40aa62c2ee646620490fb427639135511019f605
Mick <cutipus@protonmail.com>
2024-10-27 15:59:34 +0200

    Use the tailwindcss tool to generate style instead of linking to CDN

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './src/toosh_co_il/templates/**/*.html',
+    '.src/tailwind/**/*.css',
+  ],
   theme: {
+    fontFamily: {
+      narkiss: ['"narkiss-tam"', 'sans-serif'],
+    },
     extend: {},
   },
   plugins: [],
