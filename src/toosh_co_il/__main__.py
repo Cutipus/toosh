19991a4ea249b912c3407d6a82ad794f56b4765a
Mick <cutipus@protonmail.com>
2024-10-27 16:09:11 +0200

    Create a script to start dev environment for tailwind and flask

--- src/toosh_co_il/__main__.py
+++ src/toosh_co_il/__main__.py
@@ -1,6 +1,13 @@
-def main():
-    print("Please run this using `flask --app toosh_co_il run` instead!")
+import subprocess
+
+
+def start_dev_mode():
+    print("Starting dev environment")
+    subprocess.Popen(["Flask", "--debug", "--app", "toosh_co_il", "run"])
+    subprocess.Popen(
+        ["tailwindcss", "-w", "-i", "./src/tailwind/styles.css", "-o", "./src/toosh_co_il/static/dist/tailwind.css"]
+    )
 
 
 if __name__ == "__main__":
-    main()
+    start_dev_mode()
