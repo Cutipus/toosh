590905cc6362414ecb7d917fc6fc4f9952534a2a
Mick <cutipus@protonmail.com>
2024-10-27 22:50:59 +0200

    Move all images to separate project directories and automatically place them in the gallery with links

--- src/toosh_co_il/__init__.py
+++ src/toosh_co_il/__init__.py
@@ -1,16 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, abort, render_template
 
 app = Flask(__name__)
+
+gallery_format: list[list[str]] = [
+    ["juice", "maskit", "pocahontas2"],
+    ["pocahontas", "good-morning", "hibbuk1", "alefbeitgimel"],
+    ["golden-margarita-bw", "smoke", "lifta"],
+    ["sunshine", "goodgood", "hibbuk2", "pigumim"],
+]
+
+all_projects = {project for col in gallery_format for project in col}
 
 
 @app.route("/")
 def index() -> str:
-    return render_template("index.html", title="Toosh")
+    return render_template("index.html", title="Toosh", columns=gallery_format)
 
 
-@app.route("/focus/<path:text>")
-def item_focus(text: str) -> str:
-    return render_template("item-focus.html", image=text)
+@app.route("/project/<path:project_title>")
+def item_focus(project_title: str) -> str:
+    if project_title not in all_projects:
+        abort(404, "I didn't work on any project like that")
+    return render_template("item-focus.html", project=project_title)
 
 
 @app.route("/main-window")
