8a9fe1ffd419054e9c77dbef1a4c488d7d887125
Mick <cutipus@protonmail.com>
2025-08-12 03:53:18 +0300

    Prioritize downloading currently viewed image first!

--- src/toosh_co_il/static/javascript/gallery.js
+++ src/toosh_co_il/static/javascript/gallery.js
@@ -4,3 +4,9 @@ document.addEventListener("keydown", (e) => {
   if (window.location.pathname !== "/gallery") return;
   if (e.key === "Escape") document.getElementById("modal").classList.remove("currently-viewed");
 });
+
+document.addEventListener("htmx:beforeSwap", (evt) => {
+  if (evt.target.id === "modal-content") {
+    evt.target.querySelectorAll("img").forEach((img) => (img.src = ""));
+  }
+});
